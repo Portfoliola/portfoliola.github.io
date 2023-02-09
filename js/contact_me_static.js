174ac2d45929a8b41f9ca765950ae7e37cfb1157
Portfoliola <121599219+Portfoliola@users.noreply.github.com>
2023-02-09 11:05:03 -0400

    Update contact_me_static.js

--- js/contact_me_static.js
+++ js/contact_me_static.js
@@ -17,29 +17,7 @@ $(function() {
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
-            $.ajax({
-                url: "",
-                type: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
-                },
-                cache: false,
-                success: function() {
-                    // Success message
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
-
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
+            
                 error: function() {
                     // Fail message
                     $('#success').html("<div class='alert alert-danger'>");
